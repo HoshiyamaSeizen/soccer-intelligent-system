818f553fc27a0efcf1b27aff4fbf8d0909c3cc0e
HoshiyamaSeizen <nazarafanasyev@gmail.com>
2024-02-19 11:44:52 +0300

    select flags with different x

--- agent.js
+++ agent.js
@@ -1,6 +1,6 @@
 const Msg = require('./msg');
 const readline = require('readline');
-const Flags = require("./flags");
+const Flags = require('./flags');
 
 class Agent {
 	constructor() {
@@ -12,6 +12,7 @@ class Agent {
 			input: process.stdin,
 			output: process.stdout,
 		});
+		this.posMethod = '3P';
 		this.rl.on('line', (input) => {
 			// Обработка строки из кон—
 			if (this.run) {
@@ -53,37 +54,90 @@ class Agent {
 		if (p[1]) this.id = p[1]; // id игрока
 	}
 	analyzeEnv(msg, cmd, p) {
-		if (cmd == "see"){
-			let observedFlags = []
-			for (let i = 1; i < p.length; i++){
-				let flagName = p[i].cmd.p.join("");
-				if (Object.keys(Flags).includes(flagName) && p[i].p.length >= 2) { //является флагом и больше двух чисел => есть расстояние
-					observedFlags.push(p[i]);
-				} 
+		if (cmd == 'see') {
+			let observedFlags = [];
+
+			const xs = [];
+			for (let i = 1; i < p.length; i++) {
+				let flagName = p[i].cmd.p.join('');
+				if (Object.keys(Flags).includes(flagName) && p[i].p.length >= 2) {
+					let x = Flags[flagName].x;
+					if (!xs.includes(x)) {
+						observedFlags.push(p[i]);
+						xs.push(x);
+					}
+				}
 			}
-			if (observedFlags.length < 3) {console.log("Вижу меньше 3 флагов"); return;}
-			observedFlags.sort(function(a, b) { // возьмем ближайшие флаги
-				return a.p[0]-b.p[0];
-			});
+
+			if (observedFlags.length < 3 && this.posMethod === '3P')
+				return console.log('Вижу меньше 3 значимых флагов');
+			else if (observedFlags.length < 2 && this.posMethod === '2P')
+				return console.log('Вижу меньше 2 значимыхых флагов');
+
+			observedFlags.sort((a, b) => a.p[0] - b.p[0]);
 			let extractFlagCoordsAndDistance = (observedFlag) => {
-				let flagName = observedFlag.cmd.p.join("");
-				return [Flags[flagName].x, Flags[flagName].y, observedFlag.p[0]] // X, Y, расстояние до флага
-			}
+				let flagName = observedFlag.cmd.p.join('');
+				return [Flags[flagName].x, Flags[flagName].y, observedFlag.p[0]]; // X, Y, расстояние до флага
+			};
+
 			let [x1, y1, d1] = extractFlagCoordsAndDistance(observedFlags[0]);
 			let [x2, y2, d2] = extractFlagCoordsAndDistance(observedFlags[1]);
-			let [x3, y3, d3] = extractFlagCoordsAndDistance(observedFlags[2]);
-			let alpha1 = (y1 - y2) / (x2 - x1);
-			let beta1 = (y2*y2 - y1*y1 + x2*x2 - x1*x1 + d1*d1 - d2*d2) / (2 *(x2 - x1));
-			let alpha2 = (y1 - y3) / (x3 - x1);
-			let beta2 = (y3*y3 - y1*y1 + x3*x3 - x1*x1 + d1*d1 - d3*d3) / (2 *(x3 - x1));
-			let delta_beta = beta1 - beta2;
-			let delta_alpha = alpha2 - alpha1;
-			let X = alpha1*(delta_beta/delta_alpha)+beta1;
-			let Y = delta_beta/delta_alpha;
-			console.log(alpha1, alpha2, beta1, beta2, delta_alpha, delta_beta);
-			console.log("X = ", X, "Y = ", Y);
-			console.log("\n");
+			let [x3, y3, d3] =
+				this.posMethod === '3P' ? extractFlagCoordsAndDistance(observedFlags[2]) : [0, 0, 0];
+
+			let [X, Y] =
+				this.posMethod === '3P'
+					? this.calculatePos3P(x1, y1, d1, x2, y2, d2, x3, y3, d3)
+					: this.calculatePos2P(x1, y1, d1, x2, y2, d2);
+
+			console.log('X = ', X, 'Y = ', Y);
+			console.log('\n');
 		}
+	}
+	calculatePos3P(x1, y1, d1, x2, y2, d2, x3, y3, d3) {
+		let alpha1 = (y1 - y2) / (x2 - x1);
+		let beta1 = (y2 * y2 - y1 * y1 + x2 * x2 - x1 * x1 + d1 * d1 - d2 * d2) / (2 * (x2 - x1));
+		let alpha2 = (y1 - y3) / (x3 - x1);
+		let beta2 = (y3 * y3 - y1 * y1 + x3 * x3 - x1 * x1 + d1 * d1 - d3 * d3) / (2 * (x3 - x1));
+		let delta_beta = beta1 - beta2;
+		let delta_alpha = alpha2 - alpha1;
+		let X = alpha1 * (delta_beta / delta_alpha) + beta1;
+		let Y = delta_beta / delta_alpha;
+		return [X, Y];
+	}
+	calculatePos2P(x1, y1, d1, x2, y2, d2) {
+		console.log([x1, y1, d1, x2, y2, d2]);
+		const field = { l: -57.5, r: 57.5, t: 39, b: -39 };
+		let X, Y;
+
+		if (x1 !== x2) {
+			const alpha = (y1 - y2) / (x2 - x1);
+			const beta = (y2 * y2 - y1 * y1 + x2 * x2 - x1 * x1 + d1 * d1 - d2 * d2) / (2 * (x2 - x1));
+			const a = alpha * alpha + 1;
+			const b0 = x1 - beta;
+			const b = -2 * (alpha * b0 + y1);
+			const c = b0 * b0 + y1 * y1 - d1 * d1;
+
+			let Y1 = (-b + Math.sqrt(b * b - 4 * a * c)) / 2 / a;
+			let Y2 = (-b - Math.sqrt(b * b - 4 * a * c)) / 2 / a;
+			Y = Y1 >= field.b && Y1 <= field.t ? Y1 : Y2;
+
+			if (y1 === y2) {
+				X = (x2 * x2 - x1 * x1 + d1 * d1 - d2 * d2) / (x2 - x1) / 2;
+			} else {
+				let X1 = x1 + Math.sqrt(d1 * d1 - (Y - y1) * (Y - y1));
+				let X2 = x1 - Math.sqrt(d1 * d1 - (Y - y1) * (Y - y1));
+				X = X1 >= field.l && X1 <= field.r ? X1 : X2;
+			}
+		} else {
+			Y = (y2 * y2 - y1 * y1 + d1 * d1 - d2 * d2) / (y2 - y1) / 2;
+
+			let X1 = x1 + Math.sqrt(d1 * d1 - (Y - y1) * (Y - y1));
+			let X2 = x1 + Math.sqrt(d1 * d1 - (Y - y1) * (Y - y1));
+			X = X1 >= field.l && X1 <= field.r ? X1 : X2;
+		}
+
+		return [X, Y];
 	}
 	sendCmd() {
 		if (this.run) {
